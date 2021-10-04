21ad0b96a742945d1d385ba40f3b1b320cba96ca
Serafima Nerush <2002ssn@gmail.com>
2021-10-04 12:19:57 -0700

    initial commit

---
+++ RecordPro/RecordProController.swift
@@ -0,0 +1,44 @@
+//
+//  RecordProController.swift
+//  RecordPro
+//
+//  Created by Simon Ng on 11/10/2020.
+//  Copyright © 2020 AppCoda. All rights reserved.
+//
+
+import UIKit
+
+class RecordProController: UIViewController {
+
+    @IBOutlet private var stopButton: UIButton!
+    @IBOutlet private var playButton: UIButton!
+    @IBOutlet private var recordButton: UIButton!
+    @IBOutlet private var timeLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    // MARK: - Action methods
+    
+    @IBAction func stop(sender: UIButton) {
+        
+    }
+
+    @IBAction func play(sender: UIButton) {
+        
+    }
+
+    @IBAction func record(sender: UIButton) {
+        
+    }
+
+}
