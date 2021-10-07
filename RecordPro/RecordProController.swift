a96d9b9a1eb8bbfa1a540e586699f827c0bcb9b1
Serafima Nerush <2002ssn@gmail.com>
2021-10-06 19:53:47 -0700

    add record and play functionality, and timer

--- RecordPro/RecordProController.swift
+++ RecordPro/RecordProController.swift
@@ -7,15 +7,49 @@
 //
 
 import UIKit
+import AVFoundation
 
-class RecordProController: UIViewController {
+class RecordProController: UIViewController, AVAudioPlayerDelegate {
 
     @IBOutlet private var stopButton: UIButton!
     @IBOutlet private var playButton: UIButton!
     @IBOutlet private var recordButton: UIButton!
     @IBOutlet private var timeLabel: UILabel!
     
+    var audioRecorder: AVAudioRecorder!
+    var audioPlayer: AVAudioPlayer?
+    private var timer: Timer?
+    private var elapsedTimeInSecond: Int = 0
+    
+    func startTimer() {
+        timer = Timer.scheduledTimer(timeInterval: 1.0, repeats: true, block: {
+            (timer) in
+            self.elapsedTimeInSecond += 1
+            self.updateTimeLabel()
+        })
+    }
+    
+    func pauseTimer() {
+        timer?.invalidate()
+    }
+    
+    func resetTimer() {
+        timer?.invalidate()
+        elapsedTimeInSecond = 0
+        updateTimeLabel()
+    }
+    
+    func updateTimeLabel() {
+        
+        let seconds = elapsedTimeInSecond % 60
+        let minutes = (elapsedTimeInSecond / 60) % 60
+        
+        timeLabel.text = String(format: "%02d:02d", minutes, seconds)
+    }
+    
     override func viewDidLoad() {
+        
+        configure()
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
@@ -26,19 +60,144 @@ class RecordProController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    private func configure() {
+        //disable stop/play button when application launches
+        stopButton.isEnabled = false
+        playButton.isEnabled = false
+        
+        //get the document directory of user. if fails, skip to the rest of the code
+        guard let directoryURL = FileManager.default.urls(for: FileManager.SearchPathDirectory.documentDirectory, in: FileManager.SearchPathDomainMask.userDomainMask).first else {
+            
+            let alertMessage = UIAlertController(title: "Error", message: "Failed to get the document directory for recording the audio. Please try again later.", preferredStyle: .alert)
+            alertMessage.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            present(alertMessage, animated: true, completion: nil)
+            
+            return
+        }
+        
+        //set the default audio file
+        let audioFileURL = directoryURL.appendingPathComponent("MyAudioMemo.m4a")
+        
+        //setup audio session
+        let audioSession = AVAudioSession.sharedInstance()
+        
+        do {
+            try audioSession.setCategory(.playAndRecord, mode: .default, options: [ .defaultToSpeaker])
+            
+            //define the recorder setting
+            let recorderSetting: [String: Any] = [
+                AVFormatIDKey: Int(kAudioFormatMPEG4AAC),
+                AVSampleRateKey: 44100.0,
+                AVNumberOfChannelsKey: 2,
+                AVEncoderAudioQualityKey: AVAudioQuality.high.rawValue
+            ]
+            
+            //initiate and prepare recorder
+            audioRecorder = try AVAudioRecorder(url: audioFileURL, settings: recorderSetting)
+            audioRecorder.delegate = self
+            audioRecorder.isMeteringEnabled = true
+            audioRecorder.prepareToRecord()
+        } catch {
+            print(error)
+        }
+        
+    }
 
     // MARK: - Action methods
     
     @IBAction func stop(sender: UIButton) {
         
+        recordButton.setImage(UIImage(named: "Record"), for: UIControl.State.normal)
+        
+        recordButton.isEnabled = true
+        stopButton.isEnabled = false
+        playButton.isEnabled = true
+        
+        //stop the recorder
+        audioRecorder?.stop()
+        
+        let audioSession = AVAudioSession.sharedInstance()
+        
+        do {
+            try audioSession.setActive(false)
+        } catch {
+            print(error)
+        }
+        
     }
 
     @IBAction func play(sender: UIButton) {
+        
+        //initialize an instance of AVAudioPlayer with the URL of file
+        // try? is usable when you want to ignore the error (and not use do catch)
+        if !audioRecorder.isRecording {
+            guard let player = try? AVAudioPlayer(contentsOf: audioRecorder.url) else {
+                print("Failed to initialize AVAudioPlayer")
+                return
+            }
+            
+            audioPlayer = player
+            audioPlayer?.delegate = self
+            audioPlayer?.play()
+            startTimer()
+        }
         
     }
 
     @IBAction func record(sender: UIButton) {
         
+        //stop the audio player before recording
+        if let player = audioPlayer, player.isPlaying {
+            player.stop()
+        }
+        
+        if !audioRecorder.isRecording {
+            let audioSession = AVAudioSession.sharedInstance()
+            
+            do {
+                try audioSession.setActive(true)
+                
+                //start recording
+                audioRecorder.record()
+                startTimer()
+                
+                //change to the pause image
+                recordButton.setImage(UIImage(named: "Pause"), for: UIControl.State.normal)
+            } catch {
+                print(error)
+            }
+        } else {
+            // pause recording
+            
+            audioRecorder.pause()
+            
+            // change to the record image
+            recordButton.setImage(UIImage(named: "Record"), for: UIControl.State.normal)
+        }
+        
+        stopButton.isEnabled = true
+        playButton.isEnabled = false
+        
     }
 
+}
+
+extension RecordProController: AVAudioRecorderDelegate {
+    func audioRecorderDidFinishRecording(_ recorder: AVAudioRecorder, successfully flag: Bool) {
+        if flag {
+            let alertMessage = UIAlertController(title: "Finish Recording", message: "Successfully recorded the audio", preferredStyle: .alert)
+            alertMessage.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            present(alertMessage, animated: true, completion: nil)
+        }
+    }
+}
+
+extension RecordProController: AVAudioPlayerDelegate {
+    func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
+        playButton.isSelected = false
+        
+        let alertMessage = UIAlertController(title: "Finish Playing", message: "Finish playing the recording!", preferredStyle: .alert)
+        alertMessage.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        present(alertMessage, animated: true, completion: nil)
+    }
 }
